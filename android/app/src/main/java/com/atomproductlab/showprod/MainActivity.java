1d257f014a97040995120f3cfcb954a49d483caa
ShowProd Dev <atomproductlab@gmail.com>
2026-09-11 15:18:58 +0000

    fix: configura WebView identico ao Chrome no Android

--- android/app/src/main/java/com/atomproductlab/showprod/MainActivity.java
+++ android/app/src/main/java/com/atomproductlab/showprod/MainActivity.java
@@ -1,5 +1,32 @@
 package com.atomproductlab.showprod;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = getBridge().getWebView();
+        WebSettings settings = webView.getSettings();
+
+        // Força comportamento idêntico ao Chrome
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setSupportZoom(false);
+        settings.setBuiltInZoomControls(false);
+        settings.setDisplayZoomControls(false);
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setUserAgentString(settings.getUserAgentString() + " ShowProd/1.0");
+
+        // Foco no WebView
+        webView.setFocusable(true);
+        webView.setFocusableInTouchMode(true);
+        webView.requestFocus();
+    }
+}
