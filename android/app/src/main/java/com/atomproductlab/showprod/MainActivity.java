c525ede635493987d3266b0aa5941f5632f0283e
ShowProd Dev <atomproductlab@gmail.com>
2026-09-11 05:16:11 +0000

    fix: input de teclado no WebView Android
    - MainActivity: habilita foco e input no WebView explicitamente
    - capacitor.config.json: plugin Keyboard com resize body

--- android/app/src/main/java/com/atomproductlab/showprod/MainActivity.java
+++ android/app/src/main/java/com/atomproductlab/showprod/MainActivity.java
@@ -1,5 +1,29 @@
 package com.atomproductlab.showprod;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Garante que o WebView aceita input de teclado
+        WebView webView = getBridge().getWebView();
+        if (webView != null) {
+            WebSettings settings = webView.getSettings();
+            settings.setJavaScriptEnabled(true);
+            settings.setDomStorageEnabled(true);
+            settings.setDatabaseEnabled(true);
+            webView.setFocusable(true);
+            webView.setFocusableInTouchMode(true);
+            webView.requestFocus();
+        }
+    }
+}
